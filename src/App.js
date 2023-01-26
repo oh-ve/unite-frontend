0a521377efa530c811de1a4f0295952f850d96ec
oh-ve <maricic.ve@gmail.com>
2023-01-26 17:40:19 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
-import Signup from "./components/Signup";
+import Signup from "./components/SignUp";
 import Patience from "./components/Patience";
-
-import "./App.css";
 import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   const [user, setUser] = useState(null);
