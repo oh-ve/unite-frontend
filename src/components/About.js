c083f51c3e0949e46a512aa32cbb5211a513a327
Dola35 <castledoola@gmail.com>
2023-02-08 20:26:36 +0100

    initial progress on about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,101 @@
+import Ve from "../Images/Ve.jpg";
+import dola from "../Images/Dolaaa.jpeg";
+import GPT from "../Images/chat-GPT.jpg";
+import "./about.css";
+import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
 export default function About() {
   return (
     <div className="About">
-      <h1>Our Team</h1>
+      <div>
+        <h1>Our Team</h1>
+      </div>
+      <div className="card-flex">
+        <div className="card-client">
+          <div className="user-picture">
+            <img src={Ve} />
+          </div>
+          <p className="name-client">
+            Ve <span>Full-stack Developer </span>
+          </p>
+          <div class="social-media">
+            <a href="#">
+              <i>
+                <BsInstagram />
+              </i>
+              <span className="tooltip-social">Instagram</span>
+            </a>
+            <a href="#">
+              <i>
+                <BsFacebook />
+              </i>
+              <span className="tooltip-social">Facebook</span>
+            </a>
+            <a href="#">
+              <i>
+                <BsLinkedin />
+              </i>
+              <span className="tooltip-social">LinkedIn</span>
+            </a>
+          </div>
+        </div>
+        {/* --------------------------------- */}
+        <div className="card-client">
+          <div className="user-picture">
+            <img src={dola} />
+          </div>
+          <p className="name-client">
+            Dola <span>Full-stack Developer </span>
+          </p>
+          <div class="social-media">
+            <a href="#">
+              <i>
+                <BsInstagram />
+              </i>
+              <span className="tooltip-social">Instagram</span>
+            </a>
+            <a href="#">
+              <i>
+                <BsFacebook />
+              </i>
+              <span className="tooltip-social">Facebook</span>
+            </a>
+            <a href="#">
+              <i>
+                <BsLinkedin />
+              </i>
+              <span className="tooltip-social">LinkedIn</span>
+            </a>
+          </div>
+        </div>
+        <div className="card-client">
+          <div className="user-picture">
+            <img src={GPT} />
+          </div>
+          <p className="name-client">
+            Chat GPT <span>Full-stack Developer </span>
+          </p>
+          <div class="social-media">
+            <a href="#">
+              <i>
+                <BsInstagram />
+              </i>
+              <span className="tooltip-social">Instagram</span>
+            </a>
+            <a href="#">
+              <i>
+                <BsFacebook />
+              </i>
+              <span className="tooltip-social">Facebook</span>
+            </a>
+            <a href="#">
+              <i>
+                <BsLinkedin />
+              </i>
+              <span className="tooltip-social">LinkedIn</span>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
